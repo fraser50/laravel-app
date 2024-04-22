b33169419f194fafba003c80b2fe3d23046a511b
Fraser Watson <fraser.watson@mac.com>
2024-04-22 14:47:06 +0100

    Images page now links to individual images

--- resources/views/components/image-preview.blade.php
+++ resources/views/components/image-preview.blade.php
@@ -1,5 +1,7 @@
 <div class="previewBox">
+    <a href="/image/{{ $image->getKey() }}">
     <h2>{{ $image->title }}</h2>
     <img src="{{ Illuminate\Support\Facades\Storage::url($image->fileName) }}"><br>
     <p>{{ $image->description }}</p>
+    </a>
 </div>
